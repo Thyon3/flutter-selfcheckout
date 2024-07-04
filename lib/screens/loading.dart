4818de8d834ff7a6628522c486b8c79217132a47
Thyon3 <asnakemengesha80@gmail.com>
2024-07-04 14:30:00 -0400

    Add loading screen widget for app initialization

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -2,16 +2,91 @@ import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatelessWidget {
+  final String? message;
+
+  Loading({
+    this.message,
+  });
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Center(
-        child: SpinKitFadingCircle(
-          color: Colors.green,
-          size: 80.0,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SpinKitFadingCircle(
+              color: Color(0xff1faa00),
+              size: 60.0,
+            ),
+            SizedBox(height: 20.0),
+            if (message != null)
+              Text(
+                message!,
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.grey[600],
+                  fontWeight: FontWeight.w500,
+                ),
+                textAlign: TextAlign.center,
+              ),
+          ],
         ),
       ),
+    );
+  }
+}
+
+class LoadingOverlay extends StatelessWidget {
+  final Widget child;
+  final bool isLoading;
+  final String? loadingMessage;
+
+  LoadingOverlay({
+    required this.child,
+    required this.isLoading,
+    this.loadingMessage,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        child,
+        if (isLoading)
+          Container(
+            color: Colors.black.withOpacity(0.5),
+            child: Center(
+              child: Container(
+                padding: EdgeInsets.all(20.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    SpinKitFadingCircle(
+                      color: Color(0xff1faa00),
+                      size: 40.0,
+                    ),
+                    SizedBox(height: 15.0),
+                    if (loadingMessage != null)
+                      Text(
+                        loadingMessage!,
+                        style: TextStyle(
+                          fontSize: 14.0,
+                          color: Colors.black87,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+      ],
     );
   }
 }
