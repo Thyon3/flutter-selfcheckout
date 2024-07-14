35e7ebdb6a175da066d52578be1b58b4431d6dde
Thyon3 <asnakemengesha80@gmail.com>
2024-07-14 13:30:00 -0400

    Add list checkbox widget for todo items

--- lib/widgets/list_checkbox.dart
+++ lib/widgets/list_checkbox.dart
@@ -1,62 +1,128 @@
 import 'package:flutter/material.dart';
+import 'package:selfcheckoutapp/models/todo.dart';
 
-class CustomCheckbox extends StatefulWidget {
-  final bool isChecked;
-  final double size;
-  final double iconSize;
-  final Color selectedColor;
-  final Color selectedIconColor;
+class ListCheckbox extends StatefulWidget {
+  final ToDo todo;
+  final Function(bool) onCheckboxChanged;
+  final Function(ToDo) onTodoChanged;
 
-  const CustomCheckbox(
-      {this.isChecked,
-      this.size,
-      this.iconSize,
-      this.selectedColor,
-      this.selectedIconColor});
+  ListCheckbox({
+    required this.todo,
+    required this.onCheckboxChanged,
+    required this.onTodoChanged,
+  });
+
   @override
-  _CustomCheckboxState createState() => _CustomCheckboxState();
+  _ListCheckboxState createState() => _ListCheckboxState();
 }
 
-class _CustomCheckboxState extends State<CustomCheckbox> {
-  bool _isSelected = false;
+class _ListCheckboxState extends State<ListCheckbox> {
+  late bool _isChecked;
 
   @override
   void initState() {
-    _isSelected = widget.isChecked ?? false;
     super.initState();
+    _isChecked = widget.todo.complete;
+  }
+
+  void _onChanged(bool? value) {
+    setState(() {
+      _isChecked = value ?? false;
+    });
+    
+    final updatedTodo = widget.todo.toggleComplete();
+    widget.onTodoChanged(updatedTodo);
+    widget.onCheckboxChanged(_isChecked);
+  }
+
+  void _startEdit() {
+    // This would open an edit dialog or navigate to edit screen
+    print('Edit todo: ${widget.todo.title}');
+  }
+
+  void _deleteTodo() {
+    // This would show a confirmation dialog and delete the todo
+    print('Delete todo: ${widget.todo.title}');
   }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          _isSelected = !_isSelected;
-        });
-      },
-      child: AnimatedContainer(
-        duration: Duration(milliseconds: 500),
-        curve: Curves.fastLinearToSlowEaseIn,
-        decoration: BoxDecoration(
-            color: _isSelected
-                ? widget.selectedColor ?? Color(0xff1faa00)
-                : Colors.transparent,
-            borderRadius: BorderRadius.circular(2.0),
-            border: _isSelected
-                ? null
-                : Border.all(
-                    color: Colors.grey,
-                    width: 2.0,
-                  )),
-        width: widget.size ?? 20,
-        height: widget.size ?? 20,
-        child: _isSelected
-            ? Icon(
-                Icons.check,
-                color: widget.selectedIconColor ?? Colors.white,
-                size: widget.iconSize ?? 18,
-              )
-            : null,
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            spreadRadius: 1,
+            blurRadius: 3,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(12.0),
+        child: Row(
+          children: [
+            Checkbox(
+              value: _isChecked,
+              onChanged: _onChanged,
+              activeColor: Color(0xff1faa00),
+              checkColor: Colors.white,
+            ),
+            SizedBox(width: 12.0),
+            Expanded(
+              child: GestureDetector(
+                onTap: _startEdit,
+                child: Text(
+                  widget.todo.title,
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: _isChecked ? Colors.grey : Colors.black87,
+                    decoration: _isChecked 
+                        ? TextDecoration.lineThrough 
+                        : TextDecoration.none,
+                    decorationColor: _isChecked ? Colors.grey : Colors.transparent,
+                    decorationThickness: 2.0,
+                  ),
+                ),
+              ),
+            ),
+            PopupMenuButton(
+              icon: Icon(Icons.more_vert),
+              itemBuilder: (context) => [
+                PopupMenuItem(
+                  value: 'edit',
+                  child: Row(
+                    children: [
+                      Icon(Icons.edit, size: 18),
+                      SizedBox(width: 12),
+                      Text('Edit'),
+                    ],
+                  ),
+                ),
+                PopupMenuItem(
+                  value: 'delete',
+                  child: Row(
+                    children: [
+                      Icon(Icons.delete, color: Colors.red, size: 18),
+                      SizedBox(width: 12),
+                      Text('Delete', style: TextStyle(color: Colors.red)),
+                    ],
+                  ),
+                ),
+              ],
+              onSelected: (value) {
+                if (value == 'edit') {
+                  _startEdit();
+                } else if (value == 'delete') {
+                  _deleteTodo();
+                }
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
